dd210070d9137e9f01518bf1cd9d92007c481b61
Sergio gomar <sergio.gomar04@gmail.com>
2024-02-02 19:42:18 -0600

    Finish first test

--- first_test/main.py
+++ first_test/main.py
@@ -6,19 +6,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+default_width = 1400
+default_height = 1000
+
 service = Service(executable_path="C:/Users/jenif/Desktop/azulschool/python/tests/chromedriver.exe")
 driver = webdriver.Chrome(service=service)
+driver.set_window_size(default_width, default_height)
 wait = WebDriverWait(driver, 10)
 
 driver.get('https://www.google.com/')
-search_bar = wait.until(EC.presence_of_element_located((By.ID, 'APjFqb')))
-search_bar.clear()
-search_bar.send_keys('anime', Keys.ENTER)
+search_bar_google = wait.until(EC.presence_of_element_located((By.ID, 'APjFqb')))
+search_bar_google.clear()
+search_bar_google.send_keys('anime flv', Keys.ENTER)
 
-# expected_result = '"manga"'
-# actual_result = driver.find_element(By.XPATH, "//div[@class='PZPZlf ssJ7i B5dxMb']")
+link = wait.until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "AnimeFLV")))
+link.click()
 
-time.sleep(5)
+search_bar_flv = wait.until(EC.presence_of_element_located((By.ID, "search-anime")))
+search_bar_flv.send_keys('steins gate', Keys.ENTER)
 
-# assert expected_result == actual_result, f'Error. Expected result {expected_result}, and actual result is {actual_result}'
+movie = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/main/ul/li[4]/article/a')))
+movie.click()
+# movie_link = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[2]/div/main/section[2]/ul/li/a')))
+# driver.execute_script("arguments[0].scrollIntoView(true);", movie_link)
+# movie_link.click()
 
+time.sleep(10)
+driver.quit()
